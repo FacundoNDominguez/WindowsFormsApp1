ca914b3e08adc67092dff87ecacbdb7647296eb0
Facundo Nicolás Dominguez <dominguezfacundonicolas96@gmail.com>
2019-04-10 20:26:57 -0300

    Segundo Commit

--- WindowsFormsApp1/Properties/AssemblyInfo.cs
+++ WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// associated with an assembly. asd   
 [assembly: AssemblyTitle("WindowsFormsApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
